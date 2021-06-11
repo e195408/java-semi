fc4d065482ad819cf7a6e00e76105c090bf47eda
e195408 <e195408f@st.u-gakugei.ac.jp>
2021-06-11 13:06:08 +0900

    sample

--- src/main/java/controller/SampleServlet.java
+++ src/main/java/controller/SampleServlet.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,13 +15,17 @@ public class SampleServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Content-Type ヘッダの設定
-        // html を返す。文字コードは、UTF-8。
-        response.setContentType("text/html; charset=UTF-8");
-        // なんか処理してるっぽいようにしたいので、文字列を用意する。
-        String message = "Hello World!\n";
-        // 返す html を生成する。
-        PrintWriter out = response.getWriter();
-        out.println("<html>\n" + message + "</html>");
+//        // Content-Type ヘッダの設定
+//        // html を返す。文字コードは、UTF-8。
+//        response.setContentType("text/html; charset=UTF-8");
+//        // なんか処理してるっぽいようにしたいので、文字列を用意する。
+//        String message = "Hello World!\n";
+//        // 返す html を生成する。
+//        PrintWriter out = response.getWriter();
+//        out.println("<html>\n" + message + "</html>");
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/sample.jsp");
+        dispatcher.forward(request,response);
+
     }
 }
